61e1a4020e4fd85020f4cf888dd1f90926729fba
dotbenz <111028307+dotbenz@users.noreply.github.com>
2023-08-28 05:24:08 +0100

    Final Update to settings.py

--- movieApp/settings.py
+++ movieApp/settings.py
@@ -135,11 +135,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
-
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-        # ...
-    ],
 }
 
 
